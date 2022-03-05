f55607637cfc49f33e8db106a6ad5d04f8534c8f
Brand <citozibrand@gmail.com>
2022-03-05 15:52:27 +0100

    Calculative Machine

--- sumOfMissingNrs/sumOfMissingNumbers.py
+++ sumOfMissingNrs/sumOfMissingNumbers.py
@@ -10,3 +10,17 @@ def find_missing(lst):
 lst = [1, 2, 4, 6, 7, 9, 10, 12]
 # print(sum(find_missing(lst)))
 print(find_missing(lst))
+
+
+def sum_missing_numbers(list_of_numbers: list):
+    missing_numbers = []
+    _min = min(list_of_numbers)
+    _max = max(list_of_numbers)
+    for number in range(_min, _max + 1):
+        if number not in list_of_numbers:
+            missing_numbers.append(number)
+    return sum(missing_numbers)
+
+
+var = sum_missing_numbers([4, 3, 8, 1, 2]) == 18
+print(var)
